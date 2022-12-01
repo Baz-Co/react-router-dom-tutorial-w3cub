9412858d83f878ce486bd4bbe250febb7a2b078e
BazCo <sheh@bazco.de>
2022-11-30 22:43:29 -0600

    add tutorial files

---
+++ src/main.jsx
@@ -0,0 +1,56 @@
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  createRoutesFromElements
+} from "react-router-dom";
+
+import Root, { loader as RootLoader, action as RootAction } from "./routes/root";
+import Index from "./routes/index";
+import Contact, { loader as contactLoader, action as contactAction } from "./routes/contact";
+import EditContact, { action as editAction } from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
+import ErrorPage from "./error-page";
+
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: RootLoader,
+    action: RootAction,
+    children: [
+      {
+        errorElement: <ErrorPage />,
+        children: [
+          { index: true, element: <Index /> },
+          {
+            path: "contacts/:contactId",
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction,
+          },
+          {
+            path: "contacts/:contactId/edit",
+            element: <EditContact />,
+            loader: contactLoader,
+            action: editAction
+          },
+          // createRoutesFromElements(<Route path="contacts/:contactId/destroy" action={destroyAction} />),
+          {
+            path: "contacts/:contactId/destroy",
+            action: destroyAction,
+            errorElement: <div>Oops! There was an error.</div>,
+          },
+        ],
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
